bd2c3238ed2f9775b1e54a2385c31d8c310bf870
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-01-24 10:27:03 +0100

    Dénes car store

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -11,7 +11,20 @@ class CarController extends Controller
         $autok = Car::all();
         return response()->json($autok);
     }
-    public function store(){
-
+    public function store(Request $request){
+        $request->validate([
+            "modell"=>"required|string|max:255",
+            "kaucio"=>"required|integer|min:0",
+            "kilometerdij"=>"required|integer|min:0",
+            "napidij"=>"required|integer|min:0",
+            "leiras"=>"required|string|max:255"
+        ]);
+        Car::create([
+            "car_model"=>$request->modell,
+            "caution_money"=>$request->kaucio,
+            "km_price"=>$request->kilometerdij,
+            "day_price"=>$request->napidij,
+            "description"=>$request->leiras
+        ]);
     }
 }
