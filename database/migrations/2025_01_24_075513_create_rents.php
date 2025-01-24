5e903bbcb5cc9801a3b434dc2704ce60ebbca230
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2025-01-24 08:57:30 +0100

    Zoli breeze+migration2

---
+++ database/migrations/2025_01_24_075513_create_rents.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rents', function (Blueprint $table) {
+            $table->id();
+            $table->string("email");
+            $table->foreignId("car_id");
+            $table->date("rent_start");
+            $table->date("rent_end");
+            $table->integer("km");
+            $table->integer("all_price");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rents');
+    }
+};
